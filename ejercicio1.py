e4aaa52884d995d1ac89869b86a6d3874c7d9658
Juanjo <Juanjojn7@gmail.com>
2024-12-11 17:02:58 -0600

    Modificando valores

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,11 +4,13 @@ libros = {
     "El Gran Gatsby": {"autor": "F. Scott Fitzgerald", "año": 1925, "género": "Ficción", "calificación": 4.3}
 }
 
-# Imprimir detalles de un libro
 libro = "1984"
 print(f"Detalles del libro '{libro}':")
 print(f"Autor: {libros[libro]['autor']}")
 print(f"Año de publicación: {libros[libro]['año']}")
 print(f"Género: {libros[libro]['género']}")
 print(f"Calificación: {libros[libro]['calificación']}")
+
+libros["1984"]["calificación"] = 5.0
+print(f"Nueva calificación de '1984': {libros['1984']['calificación']}")
 
