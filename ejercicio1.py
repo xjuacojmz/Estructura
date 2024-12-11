622f3bdb49adeb100b518492b66da26c83fe914b
Juanjo <Juanjojn7@gmail.com>
2024-12-11 17:02:22 -0600

    Imprimiendo valores

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+libros = {
+    "1984": {"autor": "George Orwell", "año": 1949, "género": "Distopía", "calificación": 4.8},
+    "To Kill a Mockingbird": {"autor": "Harper Lee", "año": 1960, "género": "Ficción", "calificación": 4.9},
+    "El Gran Gatsby": {"autor": "F. Scott Fitzgerald", "año": 1925, "género": "Ficción", "calificación": 4.3}
+}
+
+# Imprimir detalles de un libro
+libro = "1984"
+print(f"Detalles del libro '{libro}':")
+print(f"Autor: {libros[libro]['autor']}")
+print(f"Año de publicación: {libros[libro]['año']}")
+print(f"Género: {libros[libro]['género']}")
+print(f"Calificación: {libros[libro]['calificación']}")
+
